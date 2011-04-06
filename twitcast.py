f04c25baf6976b64ec3a223c55f8e7d0f9b9f382
Krist Menina <krist@dottystylecreative.com>
2011-04-06 18:07:37 +0800

    added username

--- twitcast.py
+++ twitcast.py
@@ -9,14 +9,16 @@ api = twitter.Api()
 
 print "twitcast.py online"
 while True:
-	try:
-		status = api.GetUserTimeline('renanbarco')
-		twit = status[0].text
-		if len(twit) <= 140:		
-			os.system(" ".join([s for s in ['say',twit]])) 
-		print "waiting for next"
-		time.sleep(delay)
-	except (KeyboardInterrupt, SystemExit):
-		exit()
-	except:
-		print "Retrying"
+    try:
+        status = api.GetUserTimeline('renanbarco')
+        twit_user = status[0].user.name
+        twit = status[0].text
+        if len(twit) <= 140:		
+            print " ".join([s for s in [twit_user,'says',twit]])
+            os.system(" ".join([s for s in ['say -v Alex',twit_user,'says',twit]])) 
+        print "waiting for next"
+        time.sleep(delay)
+    except (KeyboardInterrupt, SystemExit):
+        exit()
+    except:
+        print "Retrying"
