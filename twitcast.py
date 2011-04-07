6ba1f408764e4ffb80d075b719764acf110b86d2
Krist Menina <krist@dottystylecreative.com>
2011-04-07 11:06:55 +0800

    added error handling/reporting. last tweet recall

--- twitcast.py
+++ twitcast.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python
 
-import os
+import os,sys
 import time
 import twitter
 
 delay = 900
 api = twitter.Api()
+last_tweet = ""
 
 print "twitcast.py online"
 while True:
     try:
         status = api.GetUserTimeline('renanbarco')
         twit_user = status[0].user.name
-        twit = status[0].text
-        if len(twit) <= 140:		
+        twit = str(status[0].text.decode('utf-8')).replace('\'','')
+        if last_tweet != twit and len(twit) <= 140:		
             print " ".join([s for s in [twit_user,'says',twit]])
             os.system(" ".join([s for s in ['say -v Alex',twit_user,'says',twit]])) 
+            last_tweet = twit
         print "waiting for next"
         time.sleep(delay)
     except (KeyboardInterrupt, SystemExit):
         exit()
     except:
-        print "Retrying"
+        time.sleep(300)
+        print "Retrying in 5 mins", sys.exc_info()[0]
