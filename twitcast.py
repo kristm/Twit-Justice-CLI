26922d1d3f0977656752da69198f2654ff4cd2aa
krist <krist@hellowala.org>
2011-04-07 22:37:00 +0800

    optimized unicode filter, will discard all non ascii characters. adding lovi poe

--- twitcast.py
+++ twitcast.py
@@ -8,15 +8,16 @@ import twitter
 delay = 900
 api = twitter.Api()
 last_tweet = ""
-twit_source = 'kellymisa'
+twit_source = 'lovipoe' #lovipoe, ina_feleo, kellymisa
 
 print "twitcast.py online"
 while True:
     try:
         status = api.GetUserTimeline(twit_source)
         twit_user = status[0].user.name
-        u = status[7].text.replace(u'\u201c','').replace(u'\u201d','').replace(u'\u2018','').replace(u'\u2019','').replace(u'\u263a','').replace(u'\u2665','').replace(u'\u2649','') # remove curly quotes, smiley, sad face and heart while in unicode
-        twit = re.sub(r'(\'|\"|\@)','',str(u))
+        #u = status[7].text.replace(u'\u201c','').replace(u'\u201d','').replace(u'\u2018','').replace(u'\u2019','').replace(u'\u263a','').replace(u'\u2665','').replace(u'\u2649','') # remove curly quotes, smiley, sad face and heart while in unicode
+        twit = str("".join([c for c in status[0].text if ord(c) < 128])) # ignore non ascii characters
+        #twit = re.sub(r'(\'|\"|\@)','',str(u))
         if last_tweet != twit and len(twit) <= 140:		
             print " ".join([s for s in [twit_user,'says',twit]])
             os.system(" ".join([s for s in ['say -v Alex',twit_user,'says',twit]])) 
