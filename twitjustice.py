de56c8a901ef22f1714e180295df854d81cf26bb
krist <krist@hellowala.org>
2011-04-12 21:35:09 +0800

    renamed to twitjustice

--- twitjustice.py
+++ twitjustice.py
@@ -11,6 +11,10 @@ last_tweet = ""
 twit_source = 'lovipoe' #lovipoe, ina_feleo, kellymisa
 #twit_source = 'kellymisa'
 #twit_source = 'ina_feleo'
+HAPPY_FACE = u"\u263a"
+SAD_FACE = u"\u2639"
+YIN_YANG = u"\u262f"
+HEART = u"\u2665"
 
 print "twitcast.py online"
 while True:
@@ -19,7 +23,9 @@ while True:
         status = api.GetUserTimeline(twit_source)
         twit_user = status[0].user.name
         #u = status[7].text.replace(u'\u201c','').replace(u'\u201d','').replace(u'\u2018','').replace(u'\u2019','').replace(u'\u263a','').replace(u'\u2665','').replace(u'\u2649','') # remove curly quotes, smiley, sad face and heart while in unicode
-        twit = str("".join([c for c in status[0].text if ord(c) < 128])) # ignore non ascii characters
+        msg = status[0].text
+        msg = msg.replace(HAPPY_FACE,"smiley").replace(HEART,"heart")
+        twit = str("".join([c for c in msg if ord(c) < 128])) # ignore non ascii characters
         twit = re.sub(r'(\'|\"|\@)|\)|\(|\-','',twit)
         if last_tweet != twit and len(twit) <= 140:		
             msg = " ".join([s for s in [twit_user,'says',twit,'\n']])
